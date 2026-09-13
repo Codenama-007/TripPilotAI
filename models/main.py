56ac32a4aaf44093aa32e57f99e5b01a1134dccf
coding-addict-007 <salarshahsikander@gmail.com>
2026-09-14 00:07:55 +0530

    Anakin Hackathon Project

--- models/main.py
+++ models/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://travelplanneer.netlify.app/"
         # "https://your-production-domain.com",
     ],
     allow_credentials=True,
