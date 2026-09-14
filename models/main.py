ef52b77df16ee4a37d0fcea658419b165f06ff05
coding-addict-007 <salarshahsikander@gmail.com>
2026-09-14 10:09:50 +0530

    Anakin Hackathon

--- models/main.py
+++ models/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://client-server-bay.vercel.app/"
+        "https://client-server-bay.vercel.app"
         # "https://your-production-domain.com",
     ],
     allow_credentials=True,
